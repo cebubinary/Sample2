dd282dccce5986738289589c36e3311be3226def
unknown <dgulane@CSLAB125.CSSERVER.local>
2017-09-23 19:03:38 +0800

    Visual studio 2010

---
+++ Sample/MvcApplication2/Controllers/BookController.cs
@@ -0,0 +1,64 @@
+﻿using Sample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication2.Entity;
+
+namespace Sample.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            Info info = new Info();
+            using (var x = new MyEntities())
+            {
+                var allAccount = x.Accounts.OrderBy(xxx => Guid.NewGuid()).FirstOrDefault();
+                var allBook = x.Books.ToList();
+                var allResrv = x.BookReservations.ToList();
+                info.Account.Add(allAccount);
+                info.Book.AddRange(allBook);
+                info.BookReservation.AddRange(allResrv);
+                info.BookReservationInfo.AddRange(allResrv.Select(b =>
+                    new BookReservationInfo
+                    {
+                        Id = b.Id,
+                        BookName = allBook.FirstOrDefault(book => book.BookId == b.BookId).Name,
+                        BorrowersName = x.Accounts.FirstOrDefault(account => account.AccountId == b.AccountId).Name,
+                        Qty = (int)b.Qty
+                    }));
+            }
+            return View(info);
+        }
+        public ActionResult Reserve(int AccountId, int BookId, int Qty)
+        {
+            using (var db = new MyEntities())
+            {
+                var selected = db.Accounts.FirstOrDefault(x => x.AccountId == AccountId);
+                db.BookReservations.AddObject(new BookReservation {AccountId = selected.AccountId, BookId = BookId, Qty = Qty });
+                db.SaveChanges();
+                  
+            }
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete()
+        {
+            using (var db = new MyEntities())
+            {
+
+                foreach(var ent in db.BookReservations)
+                {
+                    db.BookReservations.DeleteObject(ent);
+                }
+               
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Sample/MvcApplication2/Models/Info.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcApplication2.Entity;
+
+namespace Sample.Models
+{
+    public class Info
+    {
+        public List<Account> Account { get; set; }
+        public List<Book> Book { get; set; }
+        public List<BookReservation> BookReservation { get; set; }
+        public List<BookReservationInfo> BookReservationInfo { get; set; }
+
+        public Info()
+        {
+            Account = new List<Account>();
+            Book = new List<Book>();
+            BookReservation = new List<BookReservation>();
+            BookReservationInfo = new List<Models.BookReservationInfo>();
+        }
+    }
+
+    //public class Account {
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+    //public class Book
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public int? Qty { get; set; }
+    //}
+    public class BookReservationInfo
+    {
+        public int Id { get; set; }
+        public string BorrowersName { get; set; }
+        public string BookName { get; set; }
+        public int? Qty { get; set; }
+    }
+}
